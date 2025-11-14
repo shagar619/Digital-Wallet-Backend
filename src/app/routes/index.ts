bbc4f98d4d55aa16918905ea89d087aa84bbaf1e
shagar619 <ashagar619@gmail.com>
2025-11-14 09:19:17 +0600

    added wallet all routes functionality

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { AuthRoutes } from "../modules/auth/auth.route";
 import { UserRoutes } from "../modules/user/user.route";
+import { WalletRoutes } from "../modules/wallet/wallet.route";
+import { TransRoutes } from "../modules/transaction/transaction.route";
+import { CommissionRoutes } from "../modules/commission/commission.route";
 
 
 export const router = Router();
@@ -15,6 +18,21 @@ const moduleRoutes = [
      {
           path: "/user",
           route: UserRoutes
+     },
+
+     {
+          path: "/wallet",
+          route: WalletRoutes,
+     },
+
+     {
+          path: "/trans",
+          route: TransRoutes,
+     },
+     
+     {
+          path: "/com",
+          route: CommissionRoutes,
      },
 
 ]
