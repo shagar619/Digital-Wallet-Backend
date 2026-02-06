ab8f9932413a254b36778a8d324ff01db004222c
shagar619 <ashagar619@gmail.com>
2026-02-06 22:04:15 +0600

    modify wallet interface and model

--- src/app/modules/wallet/wallet.model.ts
+++ src/app/modules/wallet/wallet.model.ts
@@ -1,12 +1,27 @@
 import { Schema, model, Types } from "mongoose";
+import { IWallet } from "./wallet.interface";
 
 const WalletSchema = new Schema(
 {
-     user: { type: Types.ObjectId, ref: "User", required: true, unique: true },
-     balance: { type: Number, default: 50 },
-     status: { type: String, enum: ["ACTIVE", "BLOCKED"], default: "ACTIVE" },
+     user: { 
+          type: Types.ObjectId, 
+          ref: "User", required: 
+          true, unique: true 
+     },
+     balance: { 
+          type: Number, 
+          default: 50 ,
+          min: 0 // Prevent negative balance at DB level
+     },
+     status: { 
+          type: String, 
+          enum: ["ACTIVE", "BLOCKED"], 
+          default: "ACTIVE" 
+     },
 },
-{   timestamps: true, versionKey: false }
+{    timestamps: true, 
+     versionKey: false 
+}
 );
 
-export const WalletModel = model("Wallet", WalletSchema);
+export const WalletModel = model<IWallet>("Wallet", WalletSchema);
