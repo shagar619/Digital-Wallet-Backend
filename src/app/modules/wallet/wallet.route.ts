e85ae0789aef1c3cbae70b56bb3a76fe59052b40
shagar619 <ashagar619@gmail.com>
2026-02-07 22:36:01 +0600

    added update a wallet status

--- src/app/modules/wallet/wallet.route.ts
+++ src/app/modules/wallet/wallet.route.ts
@@ -68,15 +68,34 @@ import { Router } from "express";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "../user/user.interface";
 import { WalletControllers } from "./wallet.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { WalletValidations } from "./wallet.validation";
 
 
 
 const router = Router();
 
+
+// --- COMMON ROUTES ---
 router.get(
      "/my-balance",
      checkAuth(Role.USER, Role.AGENT, Role.ADMIN), // All roles have wallets
      WalletControllers.getMyBalance
+);
+
+
+// --- ADMIN ROUTES ---
+router.get(
+     "/all-wallets",
+     checkAuth(Role.ADMIN), // 🔒 Admin Only
+     WalletControllers.getAllWallets
+);
+
+router.patch(
+     "/:id",
+     checkAuth(Role.ADMIN), // 🔒 Admin Only
+     validateRequest(WalletValidations.updateWalletStatusSchema),
+     WalletControllers.updateWalletStatus
 );
 
 export const WalletRoutes = router;
